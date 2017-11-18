7bf6f8077ff4279d5dc48d8a149450716c73f433
yaniccardin <33769913+yaniccardin@users.noreply.github.com>
2017-11-17 22:02:00 -0500

    nouveau tp R

--- tp_serie.R
+++ tp_serie.R
@@ -1,6 +1,6 @@
-library('TSA')
-library('tseries')
-#allo
+#library('TSA')
+#library('tseries')
+
 #################################################
 #           TP1 SERIE CHRONO                    #
 #################################################
@@ -12,16 +12,17 @@ library('tseries')
 
 ######## importation et traitement primaire du jeu de donnees ###########
 #importation du jeu de donnees
-taux <- read.csv2("C:/Users/angag426/Desktop/nouveau/Taux_de_change_US_Euro.csv")
+taux <- read.csv2("C:/Users/TEMP/Desktop/tempo/Taux_de_change_US_Euro.csv")
 #taux <- read.csv2("C:/Users/Yanic/ulaval/Séries chronologiques/tp/Taux_de_change_US_Euro.csv")
 rendement<-taux$US.Euro
-anne.mois<-taux$Ann?e.mois
+anne.mois<-taux$Année.mois
 #on transforme en time serie
 ttaux<-ts(rendement,start=c(1999,1),end=c(2016,12),frequency = 12)
 
 ######### premiere analyse de la serie grace a son graphique et a sa fonction d'autocorrelation ##########
 #on trace le graphique
-plot(ttaux,ylab='taux de change US/Euro', type="o")
+win.graph(height=4,width=6)
+plot(ttaux,ylab='taux de change', type='o', xlab='année', main='Taux de change ')
 
 #on observe difficilement de stationnarité, 
 #forte aurocorrelation, decroissance tres lente avec l'augmentation 
@@ -38,6 +39,7 @@ BoxCox.ar(ttaux)$ci
 #d'un titre financier (ou d'un taux de change). C'est donc sans surprise qu'on trouve un lambda de 0 qui nous suggere d'utiliser
 #le logarithme de notre serie chronologique comme transformation. On s'attend egalement a avoir a differencier une seul fois
 #pour retrouve le rendement de notre taux de change
+#le rendement permet de comparer deux produits fianciers ayant des valeurs differentes entres eux
 
 plot(as.vector(log(ttaux)), type='o', ylab='logarithme du taux')
 #avec une premiere differenciation
@@ -57,7 +59,7 @@ pacf(log(ttaux))
 ar(log(ttaux))
 #R suggere d'utiliser k=2
 adf.test(log(ttaux), k=2)
-#suggere la non stationnarite avec une p value de 79%
+#suggere la non stationnarite avec une p value de 93% (tres pres detre stationnaire)
 #on tente une premiere differenciation
 ar(diff(log(ttaux)))
 #R suggere k=1
@@ -83,16 +85,40 @@ eacf(diff(log(ttaux)))
 #on serait tenter de choisir un ARMA(0,1) ce qui corroborerait les test d'autocorrelation et d'autocorrelation partiel 
 #on peut regarder la valeur de rho 0,2 du tableau (la position matricielle (1,2)) 
 #pour voir si on aurait pu la remplacer par un x ou est-ce que la position matricielle (2,2) aurait pu etre un x?
-rrr=eacf(diff(log(ttaux)))[1,2]
+rrr=eacf(diff(log(ttaux)))
 rrr$eacf[1,2]
 
 
 #le modele ARMA(0,1) corrobore la theorie du MFE, le rendement serait le rendment espere plus un bruit blanc
 # Wt = W(t-1) + et (ou Wt = log(Yt))-log(Y(t-1) = log(Yt/Y(t-1)))
 
-#ON CHOISI DONC UN MODELE ARIMA(0,1,1) pour le log de Yt
+#Qu'est-ce qui BIC en pense?
+
+plot(armasubsets(y=diff(log(ttaux)), nar=3, nma=3, y.name='test',ar.method='ols'))
+#BIC propose donc également d'utiliser ARIMA(0,1,1) mais avec et-3 plutot que et-1? wtf?
+#rendement semestriel, d'ou le t-3?
+
+#ON CHOISI DONC UN MODELE ARIMA(0,1,1) pour le log de Yt (plus simple et ressemble a MFE)
+#on va quand meme tester ARIMA(1,1,1)
 
 ####### estimation des parametres ###########
+
+#pour ARIMA(0,1,1)
+#on ne met pas la diff dans la fonction arima car cest specifier order=c(0,1,1)
+ttaux011<-arima(log(ttaux), order=c(0,1,1), method='ML')
+#intercepte nul a cause du log ou reellement nul?
+
+#pour ARIMA(1,1,1)
+ttaux111<-arima(log(ttaux), order=c(1,1,1), method='ML')
+
+#pour le ARIMA(0,1,3) propose par BIC
+#on fixe les coefficients avec l'argument fixe
+ttaux013<-arima(diff(log(ttaux)), order=c(0,1,3), method='ML', fixe=c(0, 0, NA) )
+####### etude des residus ###########
+
+#ARMA(0,1,1)  
+plot(rstandard(ttaux011), ylab='résidus standardisés', type='o')
+
 
 #################################################
 #                 QUESTION 2                     #
